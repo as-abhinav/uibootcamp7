6cd4dafb2b0283503270ba8f1a1106c4b5aed400
Abhinav Sharma <abhinavs@thoughtworks.com>
2016-11-10 09:27:34 +0530

    Tooling app for brunch

---
+++ tooling/brunch/app/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div id="content">
+        <h1>&nbsp;</h1>
+        <h2>First react application!</h2>
+        <ol>
+          <li><a href="http://brunch.io">Brunch homepage</a></li>
+          <li><a href="https://facebook.github.io/react/">React.js homepage</a></li>
+        </ol>
+      </div>
+    );
+  }
+}
