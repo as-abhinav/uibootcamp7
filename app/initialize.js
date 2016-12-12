3a403341f3060ebd4e03c873242073d24ddacd4e
Swapneel Desai <blizzard.2262@gmail.com>
2016-12-12 10:54:53 +0530

    Refactoring index.html

--- app/initialize.js
+++ app/initialize.js
@@ -2,13 +2,4 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import App from 'components/app'
 
-const load = () => {
-	ReactDOM.render(<App />, document.querySelector('#app')
-	)
-}
-
-if (document.readyState !== 'complete') {
-	document.addEventListener('DOMContentLoaded', load)
-} else {
-	load()
-}
+ReactDOM.render(<App />, document.querySelector('#app')
