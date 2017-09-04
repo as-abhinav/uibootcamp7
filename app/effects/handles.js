11fba949c6c94235bb3e3b65f2a076537242f18b
Swapneel Desai <blizzard.2262@gmail.com>
2017-09-04 20:43:33 +0530

    Making and API call to fetch tweets.

--- app/effects/handles.js
+++ app/effects/handles.js
@@ -3,4 +3,8 @@ import {receiveTweets} from '../actions/handles'
 export const fetchTweets = (handle) => {
 	// Your API call
 	return Promise.resolve(receiveTweets(handle, [{user: 'tweet 1', tweet: 'This is a test tweet'}, {user: 'tweet 2', tweet: 'This is a test tweet'}]))
+
+	//return window.fetch('http://localhost:8080/api/search?hashtag=test&tweetCount=10')
+	//.then((res) => res.json())
+	//.then((data)=> receiveTweets(handle, data))
 }
